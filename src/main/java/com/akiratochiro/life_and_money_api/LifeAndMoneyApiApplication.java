b26b02a41c4be784a3e4bd39a2530395d6224f81
Akira Tochiro <akira.tochiro23@gmail.com>
2026-09-23 14:22:50 -0300

    chore: initial Spring Boot project setup

---
+++ src/main/java/com/akiratochiro/life_and_money_api/LifeAndMoneyApiApplication.java
@@ -0,0 +1,13 @@
+package com.akiratochiro.life_and_money_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeAndMoneyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifeAndMoneyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akiratochiro/life_and_money_api/LifeAndMoneyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akiratochiro.life_and_money_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeAndMoneyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
